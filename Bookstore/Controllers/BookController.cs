872d6715c47a1215dc983e08702a4c7b9bbb7444
prashant shetye <prashantshetye2000@gmail.com>
2021-03-14 17:02:43 +1100

    Changes 1403

---
+++ Bookstore/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using Bookstore.Models;
+using Bookstore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookRepository _bookrepository;
+        public BookController(BookRepository bookrepository)
+        {
+            _bookrepository = bookrepository;
+        }
+        public IActionResult GetAllBooks()
+        {
+            return View(_bookrepository.GetAllBooks());
+        }
+
+        public IActionResult Index()
+        {
+            return View(_bookrepository.GetAllBooks());
+        }
+
+        public IActionResult GetBook(int id)
+        {
+            return View(_bookrepository.GetBookId(id));                
+        }
+
+        public List<BookModel> SearchBook(String Title, string Author)
+        {
+            return _bookrepository.SearchBook(Title, Author);
+        }
+
+    }
+}
---
+++ Bookstore/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookId(int Id) 
+        {
+            return DataSource().Where(X => X.Id == Id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(String Title, String Author)
+        {
+            return DataSource().Where(X => X.Author == Author && X.Title == Title).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id = 1, Author="Prashant", Title = "ASP" , Description = "This is the description for ASP Book" , Category="Programming", Language="English", Pages="1054"},
+                new BookModel(){ Id = 2, Author="Raj", Title = "Java" , Description = "This is the description for Java Book hello hello hello hello", Category="Framework", Language="English", Pages="2100"},
+                new BookModel(){ Id = 3, Author="Mandar", Title = "C#" , Description = "This is the description for C# Book" , Category="Coding", Language="English", Pages="1654"},
+                new BookModel(){ Id = 4, Author="John", Title = "Azure" , Description = "This is the description for Azure Book", Category="Programming", Language="English", Pages="521"}
+            };
+        }
+
+    }
+}
